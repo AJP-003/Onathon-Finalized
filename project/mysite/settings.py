732794d05d9f9454aedc94ab44cad06ef511d54c
Sera Susan Koshy <108021864+serasusan@users.noreply.github.com>
2022-09-06 23:21:28 +0530

    Update settings.py

--- project/mysite/settings.py
+++ project/mysite/settings.py
@@ -113,8 +113,12 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_ROOT = os.path.join.(BASE_DIR,'staticfiles')
+                
 
 STATIC_URL = 'static/'
 
